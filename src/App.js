19d3622cbf55097b139637e22b16008389da9d93
Maczi01 <mateusz.w.twardy@gmail.com>
2021-02-16 20:32:25 +0100

    add useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {Button, ButtonGroup, Grid, Flex} from "@chakra-ui/react"
 import {ChakraProvider} from "@chakra-ui/react"
+import {useState} from "react/cjs/react.production.min";
 
 function App() {
+    
+    const [table, setTable] = useState([1,2,3,4,5,6,7,8,9,10,11,12])
+    
+    
     return (
         <ChakraProvider>
-            <Flex direction="column" align="center" justify="space-between" width="80%" height="100%"
-                  backgroundColor="#ffd803" mx="auto" py="2%">
-                <Grid templateColumns="repeat(9, 1fr)" gap={1} w="800px"
+            <Flex direction="column" align="center" justify="space-between" width="80%" height="full"
+                  backgroundColor="#ffd803" mx="auto" py="1%">
+                <Grid templateColumns="repeat(9, 1fr)" gap="5%" w="800px" h="100%"
                 >
 
                     <Button
@@ -16,6 +21,7 @@ function App() {
                         width="48px"
                         border="2px"
                         borderColor="green.500"
+
                     >
                         1
                     </Button>
@@ -25,6 +31,7 @@ function App() {
                         width="48px"
                         border="2px"
                         borderColor="green.500"
+                        // isActive="false"
                     >
                         2
                     </Button>
