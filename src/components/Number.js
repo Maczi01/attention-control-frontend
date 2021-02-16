1f1582e803543f228650c00891532bbeec03050d
Maczi01 <mateusz.w.twardy@gmail.com>
2021-02-17 00:00:17 +0100

    Add Number component

---
+++ src/components/Number.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+import {Button, ButtonGroup, Grid, Flex} from "@chakra-ui/react"
+
+const Number = () => {
+
+    const [active, setActive] = useState(false);
+
+}
