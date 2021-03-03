9648e3a2be09295d23171f24f0250b7f89a024f9
Maczi01 <mateusz.w.twardy@gmail.com>
2021-03-04 00:42:26 +0100

    Remove unused imports

--- src/components/GameCounter.js
+++ src/components/GameCounter.js
@@ -1,10 +1,4 @@
-import React, {useState, useEffect} from "react";
-import {Button, ButtonGroup, Grid, Flex, Box} from "@chakra-ui/react"
-import {ChakraProvider} from "@chakra-ui/react"
-import Number from "./Number";
-import CurrentTimebox from "./CurrentTimebox";
-import {useDisclosure} from "@chakra-ui/react"
-import GameBoard from "./GameBoard";
+import React, {useState} from "react";
 import StartWindow from "./StartWindow";
 
 const GameCounter = ({endOfGameTime,isOpen,onClose}) => {
