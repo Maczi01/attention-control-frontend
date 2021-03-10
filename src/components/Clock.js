c669927574ceb059783ba1de07e3ec71698e2b47
Maczi01 <mateusz.w.twardy@gmail.com>
2021-03-10 21:04:38 +0100

    Remove Clock component

--- src/components/Clock.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import PropTypes from 'prop-types';
-
-const Clock = ({second})   => <span>{second}</span>
-export default Clock;
--- src/components/ResultWindow.js
+++ src/components/ResultWindow.js
@@ -2,22 +2,17 @@ import React from 'react';
 import {Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay} from "@chakra-ui/modal";
 import {Box} from "@chakra-ui/layout";
 
-const ResultWindow = ({isOpen, result = 0 }) => (
+const ResultWindow = ({isOpen, result}) => (
 
-    <Modal isOpen={isOpen} >
+    <Modal isOpen={isOpen}>
         <ModalOverlay/>
         <ModalContent>
             <ModalHeader>Attention control</ModalHeader>
             <Box p='10px' m='10px'>
                 Twoj wynik to {result}
             </Box>
-            {/*<ModalCloseButton/>*/}
             <ModalBody>
             </ModalBody>
-
-            {/*<ModalFooter>*/}
-            {/*        Startujemy!*/}
-            {/*</ModalFooter>*/}
         </ModalContent>
     </Modal>
 )
