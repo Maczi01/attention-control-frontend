101833948d9ad38e986e09bebefdd4e5d531f567
Maczi01 <mateusz.w.twardy@gmail.com>
2021-03-14 22:35:05 +0100

    Changed FetchData file, App component with load data.

--- src/App.js
+++ src/App.js
@@ -7,50 +7,44 @@ import FetchData from "./components/FetchData";
 function App() {
     const [gameData, setGameData] = useState({});
     const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState();
+    const [error, setError] = useState(null);
     const gameDataEndpoint = "https://my-project-1557350715381-default-rtdb.firebaseio.com/game.json";
     const gameResultEndpoint = "https://my-project-1557350715381-default-rtdb.firebaseio.com/game/result.json";
-    const urlToGetData = "https://my-project-1557350715381-default-rtdb.firebaseio.com/game.json";
+    // const urlToGetData = "https://my-project-1557350715381-default-rtdb.firebaseio.com/game.json";
+    const urlToGetData = "https://myject-1557350715381-default-rtdb.firebaseio.com/game.json";
 
     useEffect(async () => {
-        FetchData.getData("http://localhost:8080/api/time", 'GET')
+        FetchData.getData(urlToGetData, 'GET')
             .then(data => {
                 setGameData(data)
-                setIsPending(false);
             })
             .catch(err => {
-                setError(err)
-                setIsPending(true);
-            })
+                    setError(err.message)
+                }
+            )
+            .finally(() => setIsPending(false))
 
     }, []);
 
     const checkGivenNumber = async (number) => {
         const url = "http://localhost:8080/api";
-        return await FetchData.getData(url, 'POST', number)
+        const response = await FetchData.getData(url, 'POST', number)
+            .catch(err => {
+                setError(err)
+                console.error(err.message)
+            })
+        return await response
+
     };
 
     const getResults = async () => {
         // const url = "http://localhost:8080/api/results";
         const url = "https://my-project-1557350715381-default-rtdb.firebaseio.com/game/result.json";
-        // const response = await fetch(url,
-        // );
-        // return response.json();
-
-        // const promise = fetch(url)
-        //     .then(res => {
-        //         if (!res.ok) {
-        //             throw Error("Couldnt fetch data from server")
-        //         }
-        //         return res.json()
-        //     })
-        //     .then(data => {
-        //         return data
-        //     })
-        //     .catch(err => {
-        //         setError(err)
-        //     });
-        // console.log("its a promise" + data)
+        const response = await FetchData.getData(url, 'GET')
+            .catch(err => {
+                console.error(err.message)
+            })
+        return response
     };
 
     return (
--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,27 +1,21 @@
 const FetchData = {
     getData: async (url, method, body) => {
-        // const jsonBody = body ? JSON.stringify(body) : undefined;
-        const headers= {
+        const headers = {
             "Content-Type": "application/json",
         }
-        const response = await fetch(url,
+        return await fetch(url,
             {
-                method: method,
+                method,
                 headers,
-                body: body
+                body
             }
-            )
+        )
             .then(res => {
                 if (!res.ok) {
                     throw Error("Couldn't fetch data from server")
                 }
                 return res.json()
-            })
-        return response;
+            });
     }
 }
-
-
-//     isPending(true);
-//
 export default FetchData;
