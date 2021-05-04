66f97b532d3853e4a58d13351e348176d4049e57
matchi01 <mateusz.w.twardy@gmail.com>
2021-05-05 00:07:55 +0200

    Removed listener to ctrl+F (prevent Default)

--- src/pages/BoardView.js
+++ src/pages/BoardView.js
@@ -1,33 +1,44 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {GameDataContext} from "../context/GameDataContext";
 import GameBoard from "../components/GameBoard";
 import GameCounter from "../components/GameCounter";
-import {Flex, Spinner,Center} from "@chakra-ui/react"
+import {Flex, Spinner, Center} from "@chakra-ui/react"
 
 export const BoardView = () => {
-    const {gameData, checkGivenNumber, result, getResults, error, isPending} = useContext(GameDataContext);
-    const {board, endOfGameTime} = gameData;
+        const {gameData, checkGivenNumber, result, getResults, error, isPending} = useContext(GameDataContext);
+        const {board, endOfGameTime} = gameData;
 
-    return (
-        <Center>
-            <Flex direction="column"
-                  align="center"
-                  justify="space-around"
-                  width="80%"
-                  backgroundColor="#ffd803"
-                  mx="auto"
-            >
+    useEffect(() => {
+            window.addEventListener("keydown", e => {
+                if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+                    e.preventDefault();
+                }
+            })
+        }
+    );
 
-                <GameCounter endOfGameTime={endOfGameTime}
-                             result={result}
-                             getResults={getResults}
-                />
-                {error && <p> {error} </p>}
-                {isPending && <Spinner/>}
-                <GameBoard board={board}
-                           checkGivenNumber={checkGivenNumber}
-                />
-            </Flex>
-        </Center>
-    )
-};
+
+        return (
+            <Center>
+                <Flex direction="column"
+                      align="center"
+                      justify="space-around"
+                      width="80%"
+                      backgroundColor="#ffd803"
+                      mx="auto"
+                >
+
+                    <GameCounter endOfGameTime={endOfGameTime}
+                                 result={result}
+                                 getResults={getResults}
+                    />
+                    {error && <p> {error} </p>}
+                    {isPending && <Spinner/>}
+                    <GameBoard board={board}
+                               checkGivenNumber={checkGivenNumber}
+                    />
+                </Flex>
+            </Center>
+        )
+    }
+;
