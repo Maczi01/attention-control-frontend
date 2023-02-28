35bee8c5076cfcbb00d8ef156cbf7a2327de0fdc
MateuszTwardy <mateusz.w.twardy@gmail.com>
2023-02-28 16:48:24 +0000

    fix: added type in getResults function

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,14 +1,19 @@
 import { supabase } from '../auth/supabase';
 import { Result } from '../types/types';
 
-export const getResults = async () => {
+export const getResults = async (): Promise<Result[]> => {
   const { data, error } = await supabase.from('results').select().order('name', { ascending: false });
 
   if (error) {
     throw new Error('Error fetching results');
   }
 
-  return data;
+  return data.map((result) => ({
+    id: result.id,
+    name: result.name,
+    points: result.points,
+    accuracy: result.accuracy,
+  }));
 };
 
 export const saveResult = async (item: Result) => await supabase.from('results').insert(item).single();
